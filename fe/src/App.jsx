c308c66de0c5dfc87527bcc9ac93a2fc7bd52936
aryocypher <aryat.srivastava06@gmail.com>
2024-12-27 23:52:51 +0530

    Initial Frontend commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './components/shared/header'
+function App() {
+
+  return (
+    <>
+    <Header />
+   <div bg="light">Test</div> 
+    </>
+  )
+}
+
+export default App
